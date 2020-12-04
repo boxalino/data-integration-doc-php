106542d8b5749af176b870f189d0de6ecf98f596
danneg <dana@boxalino.com>
2020-12-04 15:42:21 +0100

    [typed] attributes inheritance

--- src/Service/Doc/Schema/Typed/NumericAttribute.php
+++ src/Service/Doc/Schema/Typed/NumericAttribute.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 namespace Boxalino\InstantUpdate\Service\Doc\Schema\Typed;
 
+use Boxalino\InstantUpdate\Service\Doc\Schema\Typed;
+
 /**
  * Class NumericAttribute
  *
