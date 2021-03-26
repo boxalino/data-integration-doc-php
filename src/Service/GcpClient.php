305eed697014b63e170b5294706fdd3d0bd7664f
danneg <dana@boxalino.com>
2021-03-26 14:11:17 +0100

    [client] trait for usability in individual projects

--- src/Service/GcpClient.php
+++ src/Service/GcpClient.php
@@ -15,6 +15,8 @@ use Psr\Log\LoggerInterface;
  */
 class GcpClient implements GcpClientInterface
 {
+    use GcpClientTrait;
+
     /**
      * @var Client
      */
@@ -30,11 +32,6 @@ class GcpClient implements GcpClientInterface
      */
     protected $environment;
 
-    /**
-     * @var
-     */
-    protected $timeout;
-
     public function __construct(LoggerInterface $logger, string $environment, int $timeout = 3)
     {
         $this->logger = $logger;
@@ -43,43 +40,41 @@ class GcpClient implements GcpClientInterface
         $this->timeout = $timeout;
     }
 
-    /**
-     * @return Client
-     */
-    public function getClient() : Client
-    {
-        return $this->client;
-    }
 
     /**
      * @param ConfigurationDataObject $configurationDataObject
-     * @param \ArrayIterator $documents
-     * @param string $mode
+     * @param \ArrayIterator $documents ([<doc_type_1>=><doc content>, <doc_type_2>=><doc_content>,..]
+     * @param string $mode (F, D, I)
      * @throws \Throwable
      */
     public function send(ConfigurationDataObject $configurationDataObject, \ArrayIterator $documents, string $mode) : void
     {
-        if($this->allowIndexing($configurationDataObject->getEndpoint()))
+        try {
+            $tm = $this->getTm();
+            $ts = $this->getMsTs();
+
+            $this->load($configurationDataObject, $documents, $mode, $tm, $ts);
+            $this->sync($configurationDataObject,$mode, $tm, $ts);
+        } catch (FailDocLoadException $exception)
         {
-            try {
-                $tm = $this->getTm();
-                $ts = $this->getMsTs();
-                $this->load($configurationDataObject, $documents, $mode, $tm, $ts);
-                $this->sync($configurationDataObject,$mode, $tm, $ts);
-            } catch (FailDocLoadException $exception)
-            {
-                throw $exception;
-            } catch (FailSyncException $exception)
-            {
-                throw $exception;
-            } catch (\Throwable $exception)
-            {
-                $this->logOrThrowException($exception);
-            }
+            throw $exception;
+        } catch (FailSyncException $exception)
+        {
+            throw $exception;
+        } catch (\Throwable $exception)
+        {
+            $this->logOrThrowException($exception);
         }
     }
 
     /**
+     * Loads every document required for the sync type to succeed:
+     *
+     * product: doc_attribute, doc_attribute_value, doc_langauge, doc_product
+     * order: doc_order
+     * user: doc_user
+     * etc
+     *
      * @param ConfigurationDataObject $configurationDataObject
      * @param \ArrayIterator $documents
      * @param string $mode
@@ -92,6 +87,7 @@ class GcpClient implements GcpClientInterface
         try{
             foreach($documents as $type => $document)
             {
+                /** if test, log the doc content (debug purposes) */
                 if($configurationDataObject->isTest())
                 {
                     $this->log($document);
@@ -116,241 +112,6 @@ class GcpClient implements GcpClientInterface
     }
 
     /**
-     * Load a document to GCP
-     *
-     * @param ConfigurationDataObject $configurationDataObject
-     * @param string $document
-     * @param string $type
-     * @param string $mode
-     * @param string $tm
-     * @param string $ts
-     */
-    public function loadDoc(ConfigurationDataObject $configurationDataObject, string $document, string $type, string $mode, string $tm, string $ts) : void
-    {
-        try{
-            $requestParameters = [
-                'Content-Type' => 'application/json',
-                GcpClientInterface::DI_REQUEST_CLIENT   => $configurationDataObject->getAccount(),
-                GcpClientInterface::DI_REQUEST_DEV      => $configurationDataObject->isDev(),
-                GcpClientInterface::DI_REQUEST_DOC      => $type,
-                GcpClientInterface::DI_REQUEST_MODE     => $mode,
-                GcpClientInterface::DI_REQUEST_TM       => $tm,
-                GcpClientInterface::DI_REQUEST_TS       => $ts
-            ];
-
-            $response = $this->getClient()->send(
-                new Request(
-                    'POST',
-                    $this->getEndpointLoad($configurationDataObject->getEndpoint()),
-                    $requestParameters,
-                    $document
-                ),
-                [
-                    'auth' => [$configurationDataObject->getApiKey(), $configurationDataObject->getApiSecret(), 'basic'],
-                    'connect_timeout' => $this->timeout
-                ]
-            );
-        } catch (\Throwable $exception)
-        {
-            if(strpos($exception->getMessage(), "timed out after"))
-            {
-                return;
-            }
-
-            if(strpos($exception->getMessage(), "413 Request Entity Too Large"))
-            {
-                $this->loadByChunk($configurationDataObject, $requestParameters, $document);
-                return;
-            }
-
-            throw new FailDocLoadException("Doc Load failed for {$configurationDataObject->getAccount()} on $mode mode at $tm with exception: " . $exception->getMessage());
-        }
-    }
-
-    /**
-     * Make a sync request once all the data sync contents have been exported
-     *
-     * @param ConfigurationDataObject $configurationDataObject
-     * @param string $mode
-     * @param string $tm
-     * @param string $ts
-     */
-    public function sync(ConfigurationDataObject $configurationDataObject, string $mode, string $tm, string $ts) : void
-    {
-        try{
-            $properties = [
-                'Content-Type' => 'application/json',
-                GcpClientInterface::DI_REQUEST_CLIENT   => $configurationDataObject->getAccount(),
-                GcpClientInterface::DI_REQUEST_DEV      => $configurationDataObject->isDev(),
-                GcpClientInterface::DI_REQUEST_TYPE     => $configurationDataObject->getType(),
-                GcpClientInterface::DI_REQUEST_PROJECT  => $configurationDataObject->getProject(),
-                GcpClientInterface::DI_REQUEST_DATASET  => $configurationDataObject->getDataset(),
-                GcpClientInterface::DI_REQUEST_MODE     => $mode,
-                GcpClientInterface::DI_REQUEST_TM       => $tm,
-                GcpClientInterface::DI_REQUEST_TS       => $ts
-            ];
-            $this->getClient()->send(
-                new Request(
-                    'POST',
-                    $this->getEndpointSync($configurationDataObject->getEndpoint()),
-                    array_filter($properties)
-                ),
-                [
-                    'auth' => [$configurationDataObject->getApiKey(), $configurationDataObject->getApiSecret(), 'basic'],
-                    'connect_timeout' => $this->timeout
-                ]
-            );
-        } catch (\Throwable $exception)
-        {
-            if(strpos($exception->getMessage(), "timed out after"))
-            {
-                return;
-            }
-            throw new FailSyncException("$mode mode sync request failed for {$configurationDataObject->getAccount()} on $tm with exception: " . $exception->getMessage());
-        }
-    }
-
-    /**
-     * Load a document to GCP
-     *
-     * @param ConfigurationDataObject $configurationDataObject
-     */
-    public function loadByChunk(ConfigurationDataObject $configurationDataObject, array $requestParameters, string $document) : void
-    {
-        try{
-            $url = $this->getLoadUrl($configurationDataObject, $requestParameters);
-            $upload = $this->getClient()->send(
-                new Request(
-                    'PUT',
-                    $url,
-                    [
-                        'Content-Type' => 'application/octet-stream'
-                    ],
-                    $document
-                ),
-                [ 'connect_timeout' => 900 ]
-            );
-
-            $uploadId = $upload->getHeader("X-GUploader-UploadID")[0];
-            $loadedSize = $upload->getHeader("x-goog-stored-content-length")[0];
-            $this->logger->info("Boxalino Data Integration finised for " . json_encode($requestParameters) . ". Code - $uploadId. Load size - $loadedSize ");
-            
-            $this->loadBq($configurationDataObject, $requestParameters);
-        } catch (\Throwable $exception)
-        {
-            if(strpos($exception->getMessage(), "timed out after"))
-            {
-                $this->logger->info("Please get in touch with Boxalino. The document took longer to be exported.". $exception->getMessage());
-                return;
-            }
-
-            throw new FailDocLoadException("Doc Load Chunk failed for " . json_encode($requestParameters) . ". Exception: " . $exception->getMessage());
-        }
-    }
-
-    /**
-     * @param ConfigurationDataObject $configurationDataObject
-     * @param array $requestParameters
-     */
-    public function loadBq(ConfigurationDataObject $configurationDataObject, array $requestParameters) : void
-    {
-        try{
-            $response = $this->getClient()->send(
-                new Request(
-                    'POST',
-                    $this->getEndpointLoadBq($configurationDataObject->getEndpoint()),
-                    $requestParameters
-                ),
-                [
-                    'auth' => [$configurationDataObject->getApiKey(), $configurationDataObject->getApiSecret(), 'basic'],
-                    'connect_timeout' => $this->timeout
-                ]
-            );
-        } catch (\Throwable $exception)
-        {
-            if(strpos($exception->getMessage(), "timed out after"))
-            {
-                return;
-            }
-
-            throw new FailDocLoadException("Doc Load failed for ". json_encode($requestParameters) . ". Exception: " . $exception->getMessage());
-        }
-    }
-
-    /**
-     * @param ConfigurationDataObject $configurationDataObject
-     * @param array $requestParameters
-     * @return string|null
-     */
-    public function getLoadUrl(ConfigurationDataObject $configurationDataObject, array $requestParameters) : ?string
-    {
-        try{
-            $signedUrlRequest = $this->getClient()->send(
-                new Request(
-                    'POST',
-                    $this->getEndpointLoadChunk($configurationDataObject->getEndpoint()),
-                    $requestParameters
-                ),
-                [
-                    'auth' => [$configurationDataObject->getApiKey(), $configurationDataObject->getApiSecret(), 'basic'],
-                    'connect_timeout' => 2
-                ]
-            );
-
-            return trim(stripslashes(rawurldecode($signedUrlRequest->getBody()->getContents())), '"');
-        } catch (\Throwable $exception)
-        {
-            throw new FailDocLoadException("Doc Load failed for ". json_encode($requestParameters) .". Exception: " . $exception->getMessage());
-        }
-    }
-
-    /**
-     * @param string $endpoint
-     * @return string
-     */
-    public function getEndpointLoad(string $endpoint) : string
-    {
-        return $endpoint . GcpClientInterface::GCP_ENDPOINT_LOAD;
-    }
-
-    /**
-     * @param string $endpoint
-     * @return string
-     */
-    public function getEndpointSync(string $endpoint) : string
-    {
-        return $endpoint . GcpClientInterface::GCP_ENDPOINT_SYNC;
-    }
-
-    /**
-     * @param string $endpoint
-     * @return string
-     */
-    public function getEndpointLoadChunk(string $endpoint) : string
-    {
-        return $endpoint . GcpClientInterface::GCP_ENDPOINT_LOAD_CHUNK;
-    }
-
-    /**
-     * @param string $endpoint
-     * @return string
-     */
-    public function getEndpointLoadBq(string $endpoint) : string
-    {
-        return $endpoint . GcpClientInterface::GCP_ENDPOINT_LOAD_BQ;
-    }
-
-    /**
-     * Check if the endpoint is available to make requests
-     *
-     * @return bool
-     */
-    public function allowIndexing(string $endpoint) : bool
-    {
-        return true;
-    }
-
-    /**
      * @param \Throwable $exception
      * @return bool
      * @throws \Throwable
@@ -358,7 +119,7 @@ class GcpClient implements GcpClientInterface
     public function logOrThrowException(\Throwable $exception): bool
     {
         if ($this->environment === 'dev') {
-            $this->logger->info($exception->getMessage());
+            $this->log($exception->getMessage());
             throw $exception;
         }
         if ($this->environment === 'prod') {
@@ -369,37 +130,5 @@ class GcpClient implements GcpClientInterface
         return false;
     }
 
-    /**
-     * @param string $message
-     */
-    public function log(string $message) : void
-    {
-        $this->logger->info($message);
-    }
-
-    /**
-     * @return string
-     */
-    public function getMsTs() : string
-    {
-        try{
-            list($usec, $sec) = explode(" ", microtime());
-            $ts = (string) ((float)$usec*1000 + (float)$sec*1000);
-
-            return substr($ts, 0, strpos($ts, '.'));
-        } catch (\Throwable $exception)
-        {
-            $ts = (new \DateTime())->getTimestamp() * 1000;
-            return (string) $ts;
-        }
-    }
-
-    /**
-     * @return string
-     */
-    public function getTm() : string
-    {
-        return gmdate("YmdHis");
-    }
 
 }
