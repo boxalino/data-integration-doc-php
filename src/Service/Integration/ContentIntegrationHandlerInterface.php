b8a6ee4235f4733c3ec69998443de2e0417ecf0b
danneg <dana@boxalino.com>
2021-04-23 19:01:11 +0200

    [content] generator and integration

---
+++ src/Service/Integration/ContentIntegrationHandlerInterface.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+namespace Boxalino\DataIntegrationDoc\Service\Integration;
+
+use Boxalino\DataIntegrationDoc\Service\Integration\Mode\FullIntegrationInterface;
+
+/**
+ * Interface ContentIntegrationHandlerInterface
+ *
+ * @package Boxalino\DataIntegrationDoc\Service\Integration
+ */
+interface ContentIntegrationHandlerInterface
+    extends IntegrationHandlerInterface, FullIntegrationInterface
+{
+
+
+}
