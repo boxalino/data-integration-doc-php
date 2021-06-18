5dfd9aa791be315a698328b546a087df8dbd5537
danneg <dana@boxalino.com>
2021-06-18 09:11:55 +0200

    [exception] new handler

---
+++ src/Service/ErrorHandler/NoRecordsFoundException.php
@@ -0,0 +1,17 @@
+<?php
+namespace Boxalino\DataIntegrationDoc\Service\ErrorHandler;
+
+/**
+ * Class NoRecordsFoundException
+ * @package Boxalino\DataIntegrationDoc\Service\ErrorHandler
+ */
+class NoRecordsFoundException extends \Error
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(string $message)
+    {
+        parent::__construct($message, 0, null);
+    }
+}
