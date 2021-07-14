0ee5bb3b6867f3d2236cb27d3671273717e74cb4
danneg <dana@boxalino.com>
2021-07-14 16:53:55 +0200

    [di property] sanitization for field names

---
+++ src/Generator/DiPropertyTrait.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+namespace Boxalino\DataIntegrationDoc\Generator;
+
+/**
+ * Trait DiPropertyTrait
+ * Common context functions to sanitize the names for the properties
+ *
+ * @package Boxalino\DataIntegrationDoc\Generator
+ */
+trait DiPropertyTrait
+{
+
+    /**
+     * Sanitize the property name in order to match the SOLR declared property
+     * '\\', '+', '-', '!', '(', ')', ':', '^', '[' , ']', '"', '{' , '}', '~', '*', '?', '|', '&', ';' , '/', ',', ' '
+     * These characters are not allowed
+     *
+     * @param string $property
+     * @return string
+     */
+    public function sanitizePropertyName(string $property) : string
+    {
+        return preg_replace("/[\s\'\,\-\+\/\!\[\]\)\(\:\^\"\{\}\~\*\?\|\&\;\,\\\]/", '_', $property);
+    }
+
+    /**
+     * @param array $properties
+     * @return array
+     */
+    public function sanitizePropertyNames(array $properties) : array
+    {
+        array_walk($properties, function($property)
+        {
+            return preg_replace("/[\s\'\,\-\+\/\!\[\]\)\(\:\^\"\{\}\~\*\?\|\&\;\,\\\]/", '_', $property);
+        });
+
+        return $properties;
+    }
+    
+
+}
