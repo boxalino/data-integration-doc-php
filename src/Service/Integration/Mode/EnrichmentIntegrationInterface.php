de79c4d2fe286273740467843698590e7d6a5fba
danneg <dana@boxalino.com>
2021-10-17 22:17:54 +0200

    [E] enrichment mode

---
+++ src/Service/Integration/Mode/EnrichmentIntegrationInterface.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+namespace Boxalino\DataIntegrationDoc\Service\Integration\Mode;
+
+/**
+ * Interface EnrichmentIntegrationInterface
+ * @package Boxalino\DataIntegrationDoc\Service\Integration\Mode
+ */
+interface EnrichmentIntegrationInterface
+{
+
+    public const INTEGRATION_MODE = "E";
+
+
+}
---
+++ src/Service/Integration/ProductEnrichmentIntegrationHandlerInterface.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+namespace Boxalino\DataIntegrationDoc\Service\Integration;
+
+use Boxalino\DataIntegrationDoc\Service\Integration\Mode\EnrichmentIntegrationInterface;
+
+/**
+ * Interface ProductDeltaIntegrationHandlerInterface
+ *
+ * @package Boxalino\DataIntegrationDoc\Service\Integration
+ */
+interface ProductEnrichmentIntegrationHandlerInterface
+    extends IntegrationHandlerInterface, EnrichmentIntegrationInterface
+{
+
+}
